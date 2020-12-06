57b4125dcf6c63079b5e4e6a8bcfae24aa1c0b5d
SanjitRao <68609739+SanjitRao@users.noreply.github.com>
2020-12-05 18:29:29 -0800

    prototype: recieives data + parse for paragraphs

--- test.py
+++ test.py
@@ -15,48 +15,57 @@ Storage (How will this data be used?)
 
 '''
 import urllib.request
-import urllib.parse
+from urllib.parse import urlparse
 import re
 import wsgiref.simple_server
 
 
-def application(environ, start_response):
-    headers = [('Content-Type', 'text/plain; charset=utf-8')]
-    start_response('200 OK', headers)
-
-    s = "environ['PATH_INFO'] = " + environ['PATH_INFO'] + '\n' + \
-        "environ['QUERY_STRING'] = " + environ['QUERY_STRING'] + '\n'
-    listPath = [];
-    for i in range(len(environ["PATH_INFO"])):
-        if(environ["PATH_INFO"][i] == "\\"):
-            j=0
-            listPath[i] = "\\"
-            while(environ["PATH_INFO"][i+j+1] != "\\"):
-                listPath[i] += environ["PATH_INFO"][i+j+1]
-                j+=1
-
-
-    return [environ["PATH_INFO"][1].encode()]
-
-
-httpd = wsgiref.simple_server.make_server('', 8000, application)
-httpd.serve_forever()
-
+#parse = urlparse(inputUrl)
+#print(parse) #[2] gives path, [1] gives main site, [4] gives Query String
+#ParseResult(scheme='https', netloc='www.youtube.com', path='/watch', params='',
+#            query='v=GEshegZzt3M&t=282s', fragment='')
 #Take in a url from the user (Won't look like this in the final version)
-print("Copy-paste URL here")
+
+
+'''values = {}
+for i in range(len(parse[4])-1): #len(query string)
+    #if "=", then values[parse[4][i-1]] += "coiiofdi943" while
+    if parse[4][i:i+2] == (parse[4][i] + "="):
+        values[parse[4][i]] = ""
+        j = 1
+        while (i+j+1 < len(parse[4])) and (parse[4][i+j+1] != "&"):
+            values[parse[4][i]] +=  parse[4][i+j+1]
+            j+=1
+        #optimization: set i = j+2
+print(values)'''
+'''url = "https://www." + parse[1]#different then the prev. url; main site + path
+data = urllib.parse.urlencode(values)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+resp = urllib.request.urlopen(req)
+respData = resp.read()'''
+
+#print(respData)
+##print(url)
+
+print("Enter a URL")
 url = input()
+try:
 
-values =
+    headers = {}
+    headers['User Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
+    req = urllib.request.Request(url, headers = headers)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+
+    paragraphs = re.findall(r'<p> (.*?) </p>', str(respData))
+    for eachP in paragraphs:
+        print(eachP)
+
+    saveFile = open('withHeaders.txt', 'w')
+    saveFile.write(str(respData))
+    saveFile.close()
 
 
-
-
-print("hello world")
-v = "tbt"
-print(v)
-print("something")
-print("My change")
-
-v= 5
-x = 10
-print(x/v)
+except Exception as e:
+    print(str(e))
