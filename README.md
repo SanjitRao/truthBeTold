4984face38142175f23eaf8b27e828c30270e333
SanjitRao <68609739+SanjitRao@users.noreply.github.com>
2020-12-24 21:25:30 -0800

    TBT interview Question

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # TruthBeTold
-#TruthBeTold is an online platform that sheds light on the accuracy of information on the Internet. Using Machine Learning and Natural Language Processing, we strive to build a platform that will combat the rise of fake news, preventing the goodwill of society from being eclipsed by the badwill of a few. 
+'''TruthBeTold is an online platform that sheds 
+light on the accuracy of information on the Internet. 
+Using Machine Learning and Natural Language Processing, 
+we strive to build a platform that will combat the rise 
+of fake news, preventing the goodwill of society from being 
+eclipsed by the badwill of a few.
+
+It will:
+
+- access the content of news channels using their API's
+- using probability, nltk, n-grams, tokenization, and 
+    (to keep things simple) the rules of the English language
+    (Syntax, grammer, semantics (nuance)), derive the message 
+    of the article
+    - There should definitely be useful modules to use
+- Based on PAST data about the website on 
+  this subject, plus some additional
+  context info about the particular topic, generate
+  a % likelihood that the info in this article is right
+- **Give the user the ability to 'rate' the score; the way they vote 
+  will marginally change the score displayed
+- prompt the user if they want to evaluate another website
+
+
+
+'''
 
 
--- test.py
+++ test.py
@@ -15,50 +15,41 @@ Storage (How will this data be used?)
 
 '''
 import urllib.request
+import urllib.parse
 from urllib.parse import urlparse
 import re
 import wsgiref.simple_server
 
-
-#parse = urlparse(inputUrl)
-#print(parse) #[2] gives path, [1] gives main site, [4] gives Query String
-#ParseResult(scheme='https', netloc='www.youtube.com', path='/watch', params='',
-#            query='v=GEshegZzt3M&t=282s', fragment='')
-#Take in a url from the user (Won't look like this in the final version)
-
-
-'''values = {}
-for i in range(len(parse[4])-1): #len(query string)
-    #if "=", then values[parse[4][i-1]] += "coiiofdi943" while
-    if parse[4][i:i+2] == (parse[4][i] + "="):
-        values[parse[4][i]] = ""
-        j = 1
-        while (i+j+1 < len(parse[4])) and (parse[4][i+j+1] != "&"):
-            values[parse[4][i]] +=  parse[4][i+j+1]
-            j+=1
-        #optimization: set i = j+2
-print(values)'''
-'''url = "https://www." + parse[1]#different then the prev. url; main site + path
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-resp = urllib.request.urlopen(req)
-respData = resp.read()'''
-
-#print(respData)
-##print(url)
-
 print("Enter a URL")
 url = input()
 try:
-
     headers = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     headers['User Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
     req = urllib.request.Request(url, headers = headers)
+    #print(req)
     resp = urllib.request.urlopen(req)
     respData = resp.read()
+    #print(respData)
 
     paragraphs = re.findall(r'<p> (.*?) </p>', str(respData))
+    print(paragraphs)
     for eachP in paragraphs:
         print(eachP)
 
