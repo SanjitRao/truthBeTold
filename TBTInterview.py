11d42cfdf872efb99e1af94e544d8927dd6ab35c
SanjitRao <68609739+SanjitRao@users.noreply.github.com>
2021-02-14 12:58:17 -0800

    Pseudocode for the TruthBeTold Algorithm! - Please work on the "Assessment of Validity" pseudocode and have it ready by next week to discuss. Good job team!

--- TBTInterview.py
+++ TBTInterview.py
@@ -1,6 +1,6 @@
 a = [[1,2,3],
      [4,5,6],
-     [7,8,9]]
+     [6,8,9]]
 
 def TBTproblem(a): #where "a" is a SQUARE 2D array
                    #(Hint: This being square is really important for this working properly)
--- firstDraft.py
+++ firstDraft.py
@@ -1,49 +1,87 @@
 ##ALL IMPORTED MODULES AND LIBRARIES
+import nltk
 
+##EXTRACTING TEXT FROM HTML AND PREPROCESSING THE DATA
+htmlTESTString = '''
+<!DOCTYPE html>
+<html>
+<body>           
+<h1> TBT Very Simple HTML </h1>
 
+<p> Humans breathe oxygen. Cats can fly. The sky is blue. </p>
+
+ Humans = breathe(oxygen)
+
+</body>
+</html>
+'''
 
 '''
-##EXTRACTING TEXT FROM HTML AND PREPROCESSING THE DATA
+DATABASE
+- how to create ==> JSON?
+- Segregate into different topcis and subjects
+    - Date
+    - https://datatofish.com/create-database-python-using-sqlite3/ <== trying to create a database
+        - segregated by country, date, and client
+        - we could segregate by date and validity?
+'''
+'''
+**import urllib.request
+response =  urllib.request.urlopen('https://en.wikipedia.org/wiki/SpaceX')
+html = response.read()
+print(html)**
+
+After extraction
+- Tokenize using nltk and/or textblob
+-Normalization: convert everything to same thing (lowercase, tense??? 
+- need DATE of data**<== allow us to determine if data is dated
+- https://www.webnots.com/how-to-find-last-updated-date-of-a-web-page/ <== check date!
 
 
+Textblob - textblob function => tokenization and spelling correction
+CoreNLP - part of teach tagging, 
+nltk to do part of speech tagging
+
+Selenium - automated html extraction
+Regex - extracting  
+from urllib.request import urlopen
+
+**from bs4 import BeautifulSoup
+soup = BeautifulSoup(html,'html5lib')
+text = soup.get_text(strip = True)
+print(text)**
+
+nltk can identify subjects and verbs (to put into Subject = Action(Object))
+- can use variables to represent parts of speech
+
+'''
 
 ##ASSESSING VALIDITY
+'''
+Form: 
+subject defined by action and object
+- Subject = Action(Object)
+    - Above form needs to be compatible with databases
+- Link pronouns to the name ("He" == "John")
+    
 
 
+- ***find websites that we can use as a credible benchmark for new information
+   - Check date!
+   - Make sure its not an opinion piece (nltk)***
+   
+CoreNLP - part of teach tagging, Full neural network pipeline for robust text analytics, including tokenization, multi-word token (MWT) expansion, lemmatization, 
+part-of-speech (POS) and morphological features tagging, dependency parsing, and named entity recognition;   
 
-##OUTPUTING THE TBT SCORE
-
-
-
-
-
-
-
-
-
-
-
+Pattern 
+Gensim 
+ 
+'''
 
 
+## OUTPUTING THE TBT SCORE
+'''
+Highlight innacurate phrases
+- Selenium: basically inspect element capabilities  
 
-
-
-    headers['User Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
-    req = urllib.request.Request(url, headers = headers)
-    #print(req)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
-    #print(respData)
-
-    paragraphs = re.findall(r'<p> (.*?) </p>', str(respData))
-    print(paragraphs)
-    for eachP in paragraphs:
-        print(eachP)
-
-    saveFile = open('withHeaders.txt', 'w')
-    saveFile.write(str(respData))
-    saveFile.close()
-
-
-except Exception as e:
-    print(str(e))
+'''
