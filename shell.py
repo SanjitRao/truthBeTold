17515542a7be9de7f80186037c8b15829000a419
avani-g <78327267+avani-g@users.noreply.github.com>
2021-03-06 23:02:40 -0800

    Added Lemmatization Code

--- shell.py
+++ shell.py
@@ -3,7 +3,16 @@ import urllib.request
 import re
 import json
 import newspaper # https://pypi.org/project/newspaper3k/
+from nltk.corpus import wordnet
+import nltk.tokenize.punkt
+from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('wordnet')
+wordnet_lemmatizer = WordNetLemmatizer()
+nltk.download('averaged_perceptron_tagger')
+
 database_list = ['Hello.', "This is a test website that doesn't do anything cool.", 'bla.']
+
 
 url = 'file:///Users/Siddharth/Desktop/Desktop/website.html'
 
@@ -23,6 +32,18 @@ text_list = sent_tokenize(text)
 
 print(text_list)
 print("\n")
+
+# Takes untokenized text and lemmatizes it
+def get_wordnet_pos(word):
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+print([wordnet_lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in (nltk.word_tokenize(text))])
+
 matched_list = []
 
 # implement ml:
