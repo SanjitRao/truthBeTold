6a0e234dca4b25a709135164bd2ae4c861fc0a81
Suma Karkala <sumakarkala@Sumas-Air.attlocal.net>
2022-01-18 22:05:46 -0800

    Organization changes to repo

--- TBT_Algorithms/nlpe2e.py
+++ TBT_Algorithms/nlpe2e.py
@@ -9,7 +9,7 @@ p1 = read_expr('man(socrates)')
 p2 = read_expr('all x.(man(x) -> mortal(x))')
 c  = read_expr('mortal(socrates)')
 Prover9().prove(c, [p1,p2]) #Uses Prover9 to logically infer that Socrates is mortal
-## TODO: Need a way to convert Strings into Expressions
+## TODO: Need way to convert Strings into Expressions
 
 
 ## TODO: make a copy of database_list, make each item an expression, set up Prover9 and equiv()
