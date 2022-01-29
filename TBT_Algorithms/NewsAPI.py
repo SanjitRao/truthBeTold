b92e6a6ec4ff21c51eafec6256a7312b2a3c5e06
Sanjit Rao <sanjitrao08@gmail.com>
2022-01-28 18:32:41 -0800

    New search alg for articles from NewsAPI

--- TBT_Algorithms/NewsAPI.py
+++ TBT_Algorithms/NewsAPI.py
@@ -1,5 +1,11 @@
+#!pip install newsapi-python
+#!pip install beautifulsoup4
+#!pip install lxml
+#!pip install requests
+
 from newsapi import NewsApiClient
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import lxml
 import requests
 import datetime as dt
@@ -9,18 +15,36 @@ import pandas as pd
 #TODO: 1) fill news_orgs_dict with ~20 news orgs.
 #      2) create a try-exception catch in case the totalResults == 0
 #      3) this function might be computationally expensive... is there a more efficient way?
-news_orgs_dict = {'bbc-news': [['meta content', 'asasdsdasd'], ['p', 'ssrcss-1q0x1qg-Paragraph eq5iqo00']]}
-def keyword_newsSource_searchAlg(keyword, news_source, news_orgs_dict=news_orgs_dict, API_KEY = 'ee08accb3d36479789b2659c9da1a59d'): # (Str, Str, Str)
-    newsapi= NewsApiClient(API_KEY)
-    data = newsapi.get_everything(q=keyword, sources=news_source, language='en')
-    text_tags = news_orgs_dict[news_source] # 'tags' should now be a list of all the text-containing tags for that particular news source
-    rv = [] # 'return value'
-    for num_article in range(data['totalResults']+1): # +1 just in case there aren't any results; gotta create a catch-exception for that
-        source_HTML = requests.get(data['articles'][num_article]['url']).text
-        soup = BeautifulSoup(source_HTML, 'lxml')
-        text_html = ''
-        for pair in text_tags:
-            text_html+= str(soup.find_all(pair[0], class_=pair[1]))
-        rv.append(text_html)
-    return rv
-# to import this function into other TBT_Algorithms files: from NewsAPI import keyword_newsSource_searchAlg
+
+api_key = 'ee08accb3d36479789b2659c9da1a59d'
+
+def keyword_NS_searchAlg_V2(keyword, news_source, API_KEY = api_key): # (Str, Str, Str)
+  newsapi= NewsApiClient(API_KEY)
+  data = newsapi.get_everything(q=keyword, sources=news_source, language='en')
+  rv = []
+  for article_num in range(data['totalResults']):
+    url = str(data['articles'][article_num]['url'])
+  #"http://news.bbc.co.uk/2/hi/health/2284783.stm"
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+# kill all script and style elements
+    for script in soup(["script", "style"]):
+      script.extract()    # rip it out
+# get text
+    text = soup.get_text()
+# break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    rv.append(text)
+  return rv
+
+#Test
+bbc_bitcoin_text = keyword_NS_searchAlg_V2('bitcoin', 'bbc-news')
+for text in bbc_bitcoin_text:
+  print(text)
+  print()
+
