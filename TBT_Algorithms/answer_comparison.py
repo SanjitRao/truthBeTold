6c00ea3189196fdc53bcfc081bcc774823e8814b
Jesse Zhao <jessez.chabot@gmail.com>
2022-02-27 12:19:27 -0800

    Add SQUAD

--- TBT_Algorithms/answer_comparison.py
+++ TBT_Algorithms/answer_comparison.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from sentence_transformers import SentenceTransformer
---
+++ TBT_Algorithms/question_answering.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+
+class tinyRobertaQA:
+    def __init__(self, checkpoint="deepset/tinyroberta-squad2"):
+        self.pipeline = pipeline('question-answering', model=checkpoint, tokenizer=checkpoint)
+    
+    def answer(self, question, context):
+        """
+        Function generates an answer based on a question and a context
+
+        Parameters:
+        question: String of the question
+        context: String of the context to which the answer will be derived fro
+
+        Returns a dictionary including the answer under the "answer" key and the score under the "score" key
+        """
+
+        QA_input = {
+            'question': question,
+            'context': context
+        }
+
+        res = self.pipeline(QA_input)
+        return res
